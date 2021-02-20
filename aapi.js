16d9fe09e8bd1cc5a20b9d79457ca9327a788f57
shahajahanferoz <shahajahanferoz@gmail.com>
2021-02-20 11:24:55 +0600

    ingredients list added

--- aapi.js
+++ aapi.js
@@ -1,3 +1,14 @@
+const searchFood=name=>{
+    console.log('name')
+
+}
+
+const searchBox=document.getElementById('search-box').value
+console.log(searchBox)
+const searchButton = document.getElementById('search-button');
+searchButton.addEventListener('click',searchFood(`${searchBox}`))
+
+
 fetch('https://www.themealdb.com/api/json/v1/1/search.php?s')
 .then(res => res.json())
 .then(data => showMenu(data.meals))
@@ -31,6 +42,16 @@ const renderFoodInfo = data =>{
         <img src="${data.strMealThumb}" class="food-image">
         <h2>${data.strMeal}</h2>
         <h4>Ingredient</h4>
+        <ul>
+            <li>${data.strMeasure1}${data.strIngredient1}</li>
+            <li>${data.strMeasure2}${data.strIngredient2}</li>
+            <li>${data.strMeasure3}${data.strIngredient3}</li>
+            <li>${data.strMeasure4}${data.strIngredient4}</li>
+            <li>${data.strMeasure5}${data.strIngredient5}</li>
+            <li>${data.strMeasure6}${data.strIngredient6}</li>
+            <li>${data.strMeasure7}${data.strIngredient7}</li>
+        </ul>
         `
-    details.innerHTML = detailsInfo;
+        
+        details.innerHTML = detailsInfo;
 }
